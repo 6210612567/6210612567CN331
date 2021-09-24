37e2425bdf773d0c32104825b8d1e65cbd286899
Nattanan Nawakitbamrung <6210612567@cn330>
2021-09-24 18:37:43 +0700

    update settings

--- university/settings.py
+++ university/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
 import os
 
 from pathlib import Path
@@ -131,3 +132,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
