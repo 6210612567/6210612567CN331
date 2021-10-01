1213f5774b1dce3a3c36b98eb8e87742ed8c2e3e
Nattanan Nawakitbamrung <6210612567@cn330>
2021-10-01 18:05:09 +0700

    delete django

--- university/settings.py
+++ university/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_heroku
+#import django_heroku
 import os
 
 from pathlib import Path
@@ -133,4 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
